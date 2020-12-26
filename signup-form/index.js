3122ebedd8bbb027198996aa4e49eee56b3a220a
edward-hong <edward.hong527@gmail.com>
2020-12-26 18:46:52 +1300

    S02L02 Single File Components

--- signup-form/index.js
+++ signup-form/index.js
@@ -1,53 +1,5 @@
-import { createApp } from 'vue/dist/vue.esm-browser.js'
+import { createApp } from 'vue'
 
-const Num = {
-  props: {
-    number: {
-      type: Number,
-      required: true,
-    },
-  },
-  template: `
-    <button :class="getClass(number)" @click="handleClick">
-      {{ number }}
-    </button>
-  `,
-  methods: {
-    handleClick() {
-      this.$emit('chosen', { number: this.number })
-    },
-    getClass(number) {
-      if (this.isEven(number)) {
-        return 'red'
-      }
-      return 'blue'
-    },
-    isEven(number) {
-      return number % 2 === 0
-    },
-  },
-}
+import App from './App.vue'
 
-const app = createApp({
-  components: {
-    Num,
-  },
-  template: `
-    <Num v-for="number in numbers" :number="number" @chosen="putInArray" />
-    <h3>Clicked number</h3>
-    <Num v-for="number in clickedNumbers" :number="number" />
-  `,
-  data() {
-    return {
-      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      clickedNumbers: [],
-    }
-  },
-  methods: {
-    putInArray(payload) {
-      this.clickedNumbers.push(payload.number)
-    },
-  },
-}).mount('#app')
-
-window.app = app
+createApp(App).mount('#app')
