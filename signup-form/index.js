0dc6018583ef994cfae4a3a84dc75f235f0942ab
edward-hong <edward.hong527@gmail.com>
2020-12-26 18:05:49 +1300

    S01L11 Your First Component

--- signup-form/index.js
+++ signup-form/index.js
@@ -1,7 +1,17 @@
 import { createApp } from 'vue/dist/vue.esm-browser.js'
 
-const app = createApp({
+const Hello = {
   template: `
+    <h3>Hello from component</h3>
+  `,
+}
+
+const app = createApp({
+  components: {
+    Hello,
+  },
+  template: `
+    <Hello />
     <button @click="increment(5)">Increment</button>
     <p>{{ count }}</p>
 
