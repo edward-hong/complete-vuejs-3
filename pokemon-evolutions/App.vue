e4ee081eb8a6fd0e9cdb6c9b040fa4c62a456ce5
edward-hong <edward.hong527@gmail.com>
2020-12-27 15:25:57 +1300

    S03L08 Reusing Code to Fetch Evolutions

--- pokemon-evolutions/App.vue
+++ pokemon-evolutions/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cards">
-    <Card v-for="starter in starters">
+    <Card v-for="starter in starters" @click="fetchEvolutions(starter)">
       <template v-slot:title>
         {{ starter.name }}
       </template>
@@ -16,36 +16,60 @@
       </template>
     </Card>
   </div>
+
+  <div class="cards">
+    <Card v-for="creature in evolutions">
+      <template v-slot:title>
+        {{ creature.name }}
+      </template>
+
+      <template v-slot:content>
+        <img :src="creature.sprite" />
+      </template>
+
+      <template v-slot:description>
+        <div v-for="type in creature.types">
+          {{ type }}
+        </div>
+      </template>
+    </Card>
+  </div>
 </template>
 
 <script>
 import Card from './Card.vue'
 
 const api = 'https://pokeapi.co/api/v2/pokemon'
-const ids = [1, 4, 7]
+const STARTER_IDS = [1, 4, 7]
 
 export default {
   components: {
     Card,
   },
 
-  created() {
-    this.fetchData()
+  async created() {
+    this.starters = await this.fetchData(STARTER_IDS)
   },
 
   data() {
     return {
       starters: [],
+      evolutions: [],
     }
   },
 
   methods: {
-    async fetchData() {
+    async fetchEvolutions(pokemon) {
+      this.evolutions = await this.fetchData([pokemon.id + 1, pokemon.id + 2])
+    },
+
+    async fetchData(ids) {
       const responses = await Promise.all(
         ids.map((id) => window.fetch(`${api}/${id}`)),
       )
       const data = await Promise.all(responses.map((res) => res.json()))
-      this.starters = data.map((datum) => ({
+      return data.map((datum) => ({
+        id: datum.id,
         name: datum.name,
         sprite: datum.sprites.other['official-artwork'].front_default,
         types: datum.types.map((type) => type.type.name),
