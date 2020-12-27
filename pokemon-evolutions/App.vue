e697062808f3e06606a8113b59ad160110f2af51
edward-hong <edward.hong527@gmail.com>
2020-12-27 15:07:37 +1300

    S03L07 Modular Components with Slots

--- pokemon-evolutions/App.vue
+++ pokemon-evolutions/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="cards">
-    <div class="card" v-for="starter in starters">
-      <div class="title">{{ starter.name }}</div>
-      <div class="content">
+    <Card v-for="starter in starters">
+      <template v-slot:title>
+        {{ starter.name }}
+      </template>
+
+      <template v-slot:content>
         <img :src="starter.sprite" />
-      </div>
-      <div class="description">
+      </template>
+
+      <template v-slot:description>
         <div v-for="type in starter.types">
           {{ type }}
         </div>
-      </div>
-    </div>
+      </template>
+    </Card>
   </div>
 </template>
 
 <script>
+import Card from './Card.vue'
+
 const api = 'https://pokeapi.co/api/v2/pokemon'
 const ids = [1, 4, 7]
 
 export default {
+  components: {
+    Card,
+  },
+
   created() {
     this.fetchData()
   },
@@ -50,30 +60,7 @@ export default {
   display: flex;
 }
 
-.card {
-  border: 1px solid silver;
-  border-radius: 8px;
-  max-width: 200px;
-  margin: 0 5px;
-  cursor: pointer;
-  box-shadow: 0px 1px 3px darkgrey;
-  transition: 0.2s;
-}
-
 img {
   width: 100%;
-}
-
-.title,
-.content,
-.description {
-  padding: 16px;
-  text-transform: capitalize;
-  text-align: center;
-}
-
-.title,
-.content {
-  border-bottom: 1px solid silver;
 }
 </style>
